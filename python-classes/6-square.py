4c632762ce9d2b63bb0d3ce78cfae3db00f38a99
Melopheelo12 <ryan12052002@gmail.com>
2026-01-26 08:25:04 +0000

    correction exo 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -41,10 +41,10 @@ class Square:
             print()
             return
 
-        # vertical offset (new lines)
+
         for _ in range(self.__position[1]):
             print()
 
-        # square printing with horizontal offset
+
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
