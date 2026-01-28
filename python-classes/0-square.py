f0a9f70b395fff89c2584e2ccbad97f963a05eb2
Melopheelo12 <ryan12052002@gmail.com>
2026-01-28 09:12:48 +0000

    Exo 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a Square class used to represent a square shape."""
 
+
 class Square:
     """This class represents a square and serves as a base for square-related behavior."""
     pass
