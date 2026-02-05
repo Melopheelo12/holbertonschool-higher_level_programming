b084ef483a2cb50dcaa9a26223ac814997178b79
Melopheelo12 <ryan1205002@gmail.com>
2026-02-05 12:42:04 +0000

    Exo6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module class BaseGeometry"""
+
+
+class BaseGeometry:
+    """basegeometry class"""
+
+    def area(self):
+        """Method for calculated area"""
+
+        raise Exception("area() is not implemented")
