4eb1b7d4d1bb6eb36037eb80ef6ba954a8c002d0
Melopheelo12 <ryan1205002@gmail.com>
2026-02-05 09:29:44 +0000

    Add comentaries

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" if the object is exactly an instance of the specified class that will
+    return true otherwise that will return false"""
+
+
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a specified class."""
 
