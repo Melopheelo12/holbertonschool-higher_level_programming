86ac81af9dd4e024920d23258caa73927ca66b1b
Melopheelo12 <ryan1205002@gmail.com>
2026-02-05 10:45:15 +0000

    Exo4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with method inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """return true if obj, a_class"""
+
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
