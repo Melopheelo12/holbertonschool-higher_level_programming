d8a1328d8dfd6485c6b62087d01fc17728bb5289
Melopheelo12 <ryan12052002@gmail.com>
2026-02-10 10:00:12 +0000

    Exo 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that append a string to a UTF-8 text file"""
+
+
+def append_write(filename="", text=""):
+    """append a string at the end of a text"""
+     with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
