58f38164d41fb6e26830fde66371d18fd861d6e4
Melopheelo12 <ryan12052002@gmail.com>
2026-03-05 12:21:47 +0000

    exo 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,14 @@
--- creates the database hbtn_0d_usa and the table cities 
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create table cities
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
