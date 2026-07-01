68d62dd278e3d25cc00f5a9368d5cef4219f5749
Melopheelo12 <ryan12052002@gmail.com>
2026-07-01 10:28:13 +0200

    .

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* This script prints multiple messages to the console. */
+
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(myVar2);
+console.log(myVar3);
