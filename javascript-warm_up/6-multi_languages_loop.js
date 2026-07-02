82d48bd1ecf1bd1fa5392b963502ad5163cff0e2
Melopheelo12 <ryan12052002@gmail.com>
2026-07-02 09:12:51 +0200

    T6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ * You must use a loop (while, for, etc.)
+ */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let out = '';
+for (let i = 0; i < arr.length; i++) {
+	out += arr[i] + '\n';
+}
+out = out.slice(0, -1);
+console.log(out);
