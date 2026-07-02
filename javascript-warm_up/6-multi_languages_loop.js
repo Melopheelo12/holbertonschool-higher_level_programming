caec1546e36aec9f58006cf19c8eec362b0ad498
Melopheelo12 <ryan12052002@gmail.com>
2026-07-02 09:43:00 +0200

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -7,7 +7,7 @@
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let out = '';
 for (let i = 0; i < arr.length; i++) {
-	out += arr[i] + '\n';
+  out += arr[i] + '\n';
 }
 out = out.slice(0, -1);
 console.log(out);
